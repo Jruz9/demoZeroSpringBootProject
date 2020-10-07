383eb50157eddbd4815e6ee55ac7a69f6bae8989
jruz9 <josec300@live.com>
2020-10-07 17:27:37 -0400

    added a new method to home controller

--- demo/demo/src/main/java/com/example/demoZero/web/HomeController.java
+++ demo/demo/src/main/java/com/example/demoZero/web/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -14,8 +15,16 @@ public class HomeController {
         model.addAttribute("formData",new User());
         return "index";  // the name of the webpage
     }
-    @RequestMapping(value = "/create",method = RequestMethod.POST)
-    public String processFormData(User user){
+    @RequestMapping(value = "/display",method = RequestMethod.GET)  //get request display the result
+    public String displayFormData(User user)
+    {
         return "result";
+    }
+    @RequestMapping(value = "/create",method = RequestMethod.POST)
+    public String processFormData(User user, RedirectAttributes attr){
+        //Insert data submitted in the form to the database.
+        //redirects dont transfer data
+        attr.addFlashAttribute("user",user);    //redirect attributes transfers data
+        return "redirect:/display";
     }
 }
