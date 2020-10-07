0cccf0e295c9bc8a102b6c57c9559e23d74a810e
jruz9 <josec300@live.com>
2020-10-07 16:09:58 -0400

    added some methods and files for practice

--- demo/demo/src/main/java/com/example/demoZero/web/UserController.java
+++ demo/demo/src/main/java/com/example/demoZero/web/UserController.java
@@ -1,11 +1,14 @@
 package com.example.demoZero.web;
 
+import com.example.demoZero.domain.Product;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -22,5 +25,16 @@ public class UserController {
         return "invoice found for user:"+userId+"on the date:"+dateOrNull;
     }
 
-
+    @RequestMapping("/{userId}/items")
+    public List<String> displayStringJson()
+    {
+        return Arrays.asList("shoes","laptop","button");
+    }
+    @RequestMapping("/{userId}/products_as_json")
+    public List<Product> displayProductsJson()
+    {
+        return Arrays.asList(new Product(1,"shoes",42.99),
+                            new Product(2,"Books",12.99),
+                            new Product(3,"bag",45.99));
+    }
 }
