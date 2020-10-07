d8cbc61d2a22de6c894d7a75a86f3c936ea07f10
jruz9 <josec300@live.com>
2020-10-07 13:19:37 -0400

    added a demo spring project and readme

---
+++ demo/demo/src/main/java/com/example/demoZero/DemoZeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoZeroApplication.class, args);
+	}
+
+}
